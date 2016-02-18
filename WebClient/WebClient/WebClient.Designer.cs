306268a1c613d80c062a4c142806ca8ddde5119f
micstach <micstach@poczta.onet.pl>
2016-02-18 18:54:00 +0100

    initial setup

---
+++ WebClient/WebClient/WebClient.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WebClient
+{
+    partial class WebClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.IsWebBrowserContextMenuEnabled = false;
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(1236, 1279);
+            this.browser.TabIndex = 0;
+            this.browser.Url = new System.Uri("http://localhost", System.UriKind.Absolute);
+            // 
+            // WebClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 1279);
+            this.Controls.Add(this.browser);
+            this.Name = "WebClient";
+            this.Text = "The-List";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser browser;
+    }
+}
+
